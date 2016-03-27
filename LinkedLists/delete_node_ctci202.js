8110eeafb7c86eeb148dcc5856e7842601c35bc4
= <danrlabrie@gmail.com>
2016-03-27 11:39:50 -0700

    delete node

---
+++ LinkedLists/delete_node_ctci202.js
@@ -0,0 +1,26 @@
+// function that deletes a node in a singly linked list given access only to that node, not the head or tail of the list.
+
+// This requires a little thought about how the linked list is actually working - we have a series of nodes which each have a reference
+// to one sibling node. So somehow we need to preserve the reference of the previous node to the node we're deleting, so that the list
+// remains unbroken, but still deleting that node.
+
+// We can do this by overwriting values of our node in question - so it still hold as a reference but assumes the value of the next
+// node in the list:
+
+
+LinkedList.prototype.deleteMe = () => {
+  if(this.next) { // as long as we're not the last node in the list.
+  this.value = this.next.value; //overwrite to next node's value
+  }
+    if(this.next.next) {  // if there is a 2nd sibling down. 
+    this.next = this.next.next; // set the next of current node to the 2nd sibling down.
+    }
+    else {
+      this.next = null; // if this is the 2nd to last node, make it the tail.
+    }
+  else {
+    this = null; // if this is already the tail, just set it to null;
+  }
+} 
+
+// cool, this is O(1) for both time and space complexity.
--- LinkedLists/kth_to_last_ctci202.js
+++ LinkedLists/kth_to_last_ctci202.js
@@ -1,44 +1,49 @@
-// function that returns the kth to last element of a linked list.
+// function that returns the kth to last element in a linked list.
 
-// there are a few things that would make this a trivial problem. mainly - if we're given the tail and the list is doubly-linked.
-// or if we're given the length of the ll from the get.
 
-// so lets assume we have a signly linked list, and we don't know the length.
+// this is trivial if we're given the length of the linked list beforehand, or the tail, so lets assume we're not.
+// we could also pretty trivially just get the length in one pass and then get the kth to last element in the next. but we can do a
+// little better by using 2 pointers
 
-LinkedList.prototype.kthToLast = (k) => {
-  let pointer1 = this.head,
-  pointer2 = this.head,
-  i=k,
-  length_from_k;
-  while(i) {
-    if(!pointer1) return "Not Enough Nodes!"
+//move both pointers out to k
+// iterate one pointer to end, record length from k.
+// iterate the other pointer length - k times. return that pointer.
+// if length from k is less than k iterate from head k-length_from_k times.
+
+LinkedList.prototype.kth_to_last = (k) => {
+  let pointer1 = this.head;
+  let pointer2 = this.head;
+  let length_from_k = 0;
+  let l = k;
+  // first we move our pointers out to k.
+  while(l>0) {
+    if(!pointer2) {
+      return "List is not that long."
+    }
     pointer2 = pointer2.next;
     pointer1 = pointer1.next;
-    i--;
+    l--;
   }
-  while(pointer2) {
-    pointer2 = pointer2.next;
+
+  while(pointer1) {
+    pointer1 = pointer1.next
     length_from_k++;
   }
-  if(length_from_k < k) {
-    pointer1 = this.head;
-    while(k - length_from_k) {
+
+  if(length_from_k > k) {
+    while(length_from_k - k) {
+      pointer2 = pointer2.next;
+      length_from_k--;
+    }
+  else
+    pointer1 = this.head; 
+    while(k-length_from_k) {
       pointer1 = pointer1.next;
-      length_from_k--
-    }
-  }
-  else {
-    while(length_from_k -k) {
       k--;
-      pointer1=pointer1.next;
     }
   }
-  return pointer1;
 }
 
-// so here we first iterate both pointers to k
-// then the 2nd pointer to the end of the list, recording the distance from k
-// if the distance is less than k. we restart pointer1 from the beginning and move it k-lfk iterations. returning that node.
-// otherwise we iterate lfk -k from k to get our node k nodes from the end and return it at the end of the world.
-// this is MAYBE a bit optimized over just getting the full length and then iterating from the beginning, if we hit the 2nd case.
+// this is O(2n) = O(n) worst case, but we save a little bit of time in the case that our length_from_k is longer than k,
+// having iterated to k to begin with. O(1) space
 
