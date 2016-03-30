f61b07e68df85c30a55e8917c7fea4968cbb4d07
= <danrlabrie@gmail.com>
2016-03-30 11:57:15 -0700

    ll palindrome

---
+++ LinkedLists/ll_palindrome_ctci206.js
@@ -0,0 +1,27 @@
+// function that returns a boolean true/false if a linked list is a palindrome or not.
+
+// we can use a two pass approach here. create a reverse copy of the list, then iterate through both to midpoint and compare.
+
+
+LinkedList.prototype.isPalindrome = () => {
+  let current = this.head;
+  let reversell = new LinkedList();
+  let linkedListLength = 0;
+  while(current) {
+    reversell.addToHead(current.data)
+    current = current.next;
+    linkedListLength++
+  }
+
+  current = this.head;
+  let reverseCurrent = reversell.head;
+  for(var i=0; i<Math.ceil(linkedListLength/2); i++) {
+    if(reverseCurrent.data !== current.data) return false;
+    current = current.next;
+    reverseCurrent = current.next;
+  }
+  return true;
+};
+
+// O(n) time and space.
+
